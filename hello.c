7668e2607873f33db7e2d41c10be57609f112377
onurdev <onurbarisdev@hotmail.com>
2015-04-24 20:02:33 +0300

    CHANGE the dummy program hello.c while trying to figure out how to parse a string

--- hello.c
+++ hello.c
@@ -1,4 +1,42 @@
 #include <stdio.h>
+
+
+
+char** set(){
+char** args=calloc(2 , sizeof(char*));
+int i,j;
+for(i=0;i<2;++i){
+		args[i] = calloc(5 , sizeof(char));
+		for (j = 0; j < 3; ++j)
+		{	
+		char* s="a";;
+		args[i][j]=s[0];
+		printf("%c, ",s[0]);
+		}
+		args[i][3]='\0';
+	}
+	printf("\n\n\n");
+	return args;
+}
+
+
 int main(){
-printf("\nHello World!\n");
+	char **args;
+	//args=calloc(2 , sizeof(char*));	
+	args=set();
+	int i,j;
+
+	for(i=0;i<2;++i){
+		for (j = 0; j < 3; ++j)
+		{	
+		printf("%c, ",args[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n\n\n");
+	
+	for ( i = 0; i < 2; ++i)
+	{
+		printf("%s\n", args[i]);
+	}
 }
